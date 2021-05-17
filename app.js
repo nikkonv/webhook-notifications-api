b0c59c68c7129062a26c9688da116dc3c51e052c
Nicolas Navarrete M <nikkolasnv1997@gmail.com>
2021-05-16 23:02:15 -0400

    first try

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('This will receive webhooks')
+})
+
+app.post('/notifications', (req, res) => {
+  console.log(req.body)
+  res.status(200).end()
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
